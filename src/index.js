955043aa00a2f9286c720871f814ee51b9e799ef
The-Normal-One1 <manermidem@gmail.com>
2022-11-02 18:37:22 +0300

    Modify the src folder for the project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 import _ from 'lodash';
+
+// eslint-disable-line
+
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
+const dolist = [
+    {
+      index: 0,
+      description: 'Doing some exercise',
+      completed: true,
+    },
+    {
+      index: 1,
+      description: 'Meeting with family',
+      completed: true,
+    },
+    {
+      index: 2,
+      description: 'Watching football games',
+      completed: true,
+    },
+    {
+      index: 3,
+      description: 'Study',
+      completed: true,
+    },
+  ];
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'Ermiyas'], ' ');
-    element.classList.add('hello');
+  const taskInput = document.querySelector('.do-list');
 
-    return element;
-}
+  const lists = () => {
+    const list = dolist.map((items) =>
+    `<div class="item">
+        <div class="btn-check">
+        <i class="fa-regular fa-square"></i>
+        </div>
+        <p>${items.description}</p>
+        <div class="task-dots">
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+        <i class="fa-solid fa-trash"></i>
+        </div>
+    </div>`).join('');
 
-document.body.appendChild(component());
+taskInput.insertAdjacentHTML('afterend', list);
+  };
+
+  window.document.addEventListener('DOMContentLoaded', lists);
+
+
+// function component() {
+//     const element = document.createElement('div');
+
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'Ermiyas'], ' ');
+//     element.classList.add('hello');
+
+//     return element;
+// }
+
+// document.body.appendChild(component());
