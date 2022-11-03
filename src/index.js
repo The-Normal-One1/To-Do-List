e25a84a726c4e30c4366d0a8c974a1c1c1dac912
The-Normal-One1 <manermidem@gmail.com>
2022-11-03 23:23:03 +0300

    Check liners error

--- src/index.js
+++ src/index.js
@@ -1,137 +1,129 @@
 // import _ from 'lodash';
 
-import form from './modules/do';
-import { todoInput } from './modules/do';
-import { doList } from './modules/do';
+import form, { todoInput, doList } from './modules/do.js';
 
 // eslint-disable-line
 
 import './style.css';
 
-
 // Vars
 let todos = JSON.parse(localStorage.getItem('todos')) || [];
 let editTodoId = -1;
 
-// Form submit
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+// render todos
 
-    saveTodo();
-    renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos));
-});
+const renderTodos = () => {
+  if (todos.length === 0) {
+    doList.innerHTML = '<center>Nothing To Do!</center>';
+    return;
+  }
+
+  // clear element before re render
+  doList.innerHTML = '';
+
+  todos.forEach((todo, index) => {
+    doList.innerHTML += `
+          <div class="item" id=${index}>
+                  <i class="fa-regular ${todo.completed ? 'fa-square-check' : 'fa-square'}"
+                  data-action='check'
+                  ></i>
+                  <p data-action='edit'>${todo.description}</p>
+                  <i class="fa-solid fa-trash" data-action='delete'>                <i class="fa-solid fa-ellipsis-vertical" data-action='move'></i>
+                  </i>
+          </div>`;
+  });
+};
 
 // Save todo
 
 const saveTodo = () => {
-    const todoValue = todoInput.value;
+  const todoValue = todoInput.value;
 
-    // check if the todolist is empty
-    const isEmpty = todoValue === '';
+  // check if the todolist is empty
+  const isEmpty = todoValue === '';
 
-    // check for duplication
-    const isDuplicate = todos.some((todo) => todo.description.toUpperCase() === todoValue.toUpperCase())
+  if (isEmpty) {
+    alert('To do list is empty');
+  } else if (todos.some((todo) => todo.description.toUpperCase() === todoValue.toUpperCase())) {
+    alert('Todo already exists!');
+  } else {
+    if (editTodoId >= 0) {
+      // update the edit todo
+      todos = todos.map((todo, index) => ({
+        ...todo,
+        description: index === editTodoId ? todoValue : todo.description,
+      }));
 
-    if (isEmpty) {
-        alert("To do list is empty");
-    } else if (isDuplicate) {
-        alert('Todo already exists!');
+      editTodoId = -1;
     } else {
-        if (editTodoId >= 0) {
-            // update the edit todo
-            todos = todos.map((todo, index) => ({
-                    ...todo,
-                description: index ===  editTodoId ? todoValue : todo.description
-            }));
+      todos.push({
+        indexNum: `@${Math.floor(Math.random() * todos.length)}`,
+        description: todoValue,
+        completed: false,
+      });
+    }
+    todoInput.value = '';
+  }
+};
 
-            editTodoId = -1;
-        } else {
-            todos.push({
-                indexNum: '@'+ Math.floor(Math.random() * todos.length),
-                description: todoValue,
-                completed: false,
-            });
-        }
-        todoInput.value = '';
-    };
-}
+// Form submit
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-
-// render todos
-
-const renderTodos = () => {
-if (todos.length === 0) {
-    doList.innerHTML =`<center>Nothing To Do!</center>`
-    return
-}
-
-    // clear element before re render
-    doList.innerHTML = '';
-
-    todos.forEach((todo, index) => {
-        doList.innerHTML += `
-        <div class="item" id=${index}>
-                <i class="fa-regular ${todo.completed ? 'fa-square-check' : 'fa-square'}"
-                data-action='check'
-                ></i>
-                <p data-action='edit'>${todo.description}</p>
-                <i class="fa-solid fa-trash" data-action='delete'>                <i class="fa-solid fa-ellipsis-vertical" data-action='move'></i>
-                </i>
-        </div>`;
-    })
-}
+  saveTodo();
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+});
 
 // first render
 
 renderTodos();
 
-
-// add eventlisner for all the do lists
-
-doList.addEventListener('click', (e) => {
-    const target = e.target;
-    const parentEle = target.parentNode;
-
-    if (parentEle.className !== 'item') return;
-
-    const item = parentEle;
-    const itemId = Number(item.id);
-
-    const action = target.dataset.action;
-
-    action === 'check' && checkTodo(itemId);
-    action === 'edit' && editTodo(itemId);
-    // action === 'move' && moveTodo(itemId);
-    action === 'delete' && deleteTodo(itemId);
-});
-
 // check for todo
 
 const checkTodo = (itemId) => {
-    todos = todos.map((todo, index) => ({
-        ...todo,
-        completed: index === itemId? !todo.completed : todo.completed
-    }));
+  todos = todos.map((todo, index) => ({
+    ...todo,
+    completed: index === itemId ? !todo.completed : todo.completed,
+  }));
 
-    renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos));
-}
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
 
 // edit todo
 
 const editTodo = (itemId) => {
-    todoInput.value = todos[itemId].description;
-    editTodoId = itemId
-}
+  todoInput.value = todos[itemId].description;
+  editTodoId = itemId;
+};
 
 // delete todo
 
 const deleteTodo = (itemId) => {
-    todos = todos.filter((todo, index) => index !== itemId);
-    editTodoId = -1;
+  todos = todos.filter((todo, index) => index !== itemId);
+  editTodoId = -1;
 
-    // re render
-    renderTodos();
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
+  // re render
+  renderTodos();
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+// add eventlisner for all the do lists
+
+doList.addEventListener('click', (e) => {
+  const { target } = e;
+  const parentEle = target.parentNode;
+
+  if (parentEle.className !== 'item') return;
+
+  const item = parentEle;
+  const itemId = Number(item.id);
+
+  const { action } = target.dataset;
+
+  if (action === 'check' && checkTodo(itemId)) return;
+  if (action === 'edit' && editTodo(itemId)) return;
+  // action === 'move' && moveTodo(itemId);
+  if (action === 'delete' && deleteTodo(itemId));
+});
--- src/modules/do.js
+++ src/modules/do.js
@@ -2,5 +2,5 @@
 
 const form = document.querySelector('.form');
 export const todoInput = document.querySelector('#list-input');
-export const doList  = document.querySelector('.do-list');
+export const doList = document.querySelector('.do-list');
 export default form;
