31a91ce9795cdd882468a87217e9c10332956e83
The-Normal-One1 <manermidem@gmail.com>
2022-11-11 16:27:57 +0300

    Check for linters error

--- src/modules/add.test.js
+++ src/modules/add.test.js
@@ -1,26 +1,17 @@
 // const addition = require('./calcul.js')
-import Tasks, {todos} from './tasks.js';
+import Tasks, { todos } from './tasks.js';
 
 describe('Check adding todos', () => {
-    test('Add todos functionality', ()=>{
-        Tasks.saveTodo('To read about JavaScript')
-        expect(todos).toHaveLength(1)
-        })
-    test('addition test', ()=>{
-        Tasks.saveTodo('Meeting with friends')
-        expect(todos).toHaveLength(2)
-        })
-    test('Checking added todos', ()=>{
-        Tasks.saveTodo('Playing football')
-        expect(todos[2].description).toBe('Playing football')
-        })
+  test('Add todos functionality', () => {
+    Tasks.saveTodo('To read about JavaScript');
+    expect(todos).toHaveLength(1);
   });
-
-// beforeAll(() => {
-//     document.body.innerHTML =
-//       "<div>" +
-//       '  <input id="addTask" type="text" placeholder="Add a task ..."/>' +
-//       "  <ul> </ul>" +
-//       '  <div><span id="count">0</span> tasks left to do</div>' +
-//       "</div>";
-//     });
+  test('addition test', () => {
+    Tasks.saveTodo('Meeting with friends');
+    expect(todos).toHaveLength(2);
+  });
+  test('Checking added todos', () => {
+    Tasks.saveTodo('Playing football');
+    expect(todos[2].description).toBe('Playing football');
+  });
+});
