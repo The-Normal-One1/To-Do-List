d87b2728083b6e28cf2cb6407445a263bbf4d2a5
The-Normal-One1 <manermidem@gmail.com>
2022-11-15 10:57:20 +0300

    Fix deployment in the code

--- src/modules/remove.test.js
+++ src/modules/remove.test.js
@@ -1,5 +1,5 @@
-import { upperCase } from 'lodash';
-import Tasks, { todos } from './tasks.js';
+// import { upperCase } from 'lodash';
+import Tasks1, { todos } from './tasks.js';
 
 const setup = {
   appConfig: jest.fn(),
@@ -8,13 +8,15 @@ Object.defineProperty(window, 'setup', setup);
 
 describe('Check removing todos', () => {
   test('Remove todos from the list', () => {
-    const Tasks = jest.spyOn(Tasks, 'tasks');
+    const Tasks = jest.spyOn(Tasks1, 'saveTodo');
+    const Delete = jest.spyOn(Tasks1, 'deleteTodo');
+    const Render = jest.spyOn(Tasks1, 'renderTodos');
     // const doList = document.querySelector('.do-list');
-    Tasks.saveTodo('To read about JavaScript');
-    Tasks.saveTodo('Meeting with friends');
-    Tasks.saveTodo('Meeting');
-    Tasks.deleteTodo(0);
-    Tasks.renderTodos(Tasks.doList);
-    expect(todos).toHaveLength(2);
+    Tasks('To read about JavaScript');
+    Tasks('Meeting with friends');
+    Tasks('Meeting');
+    Delete(0);
+    Render(Tasks1.doList);
+    expect(Tasks1.todos).toHaveLength(2);
   });
 });
