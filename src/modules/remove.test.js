0318a17a32992afe4a03cf1c3931d94df740e024
The-Normal-One1 <manermidem@gmail.com>
2022-11-15 10:40:52 +0300

    Fix deployment

--- src/modules/remove.test.js
+++ src/modules/remove.test.js
@@ -1,3 +1,4 @@
+import { upperCase } from 'lodash';
 import Tasks, { todos } from './tasks.js';
 
 const setup = {
@@ -7,12 +8,13 @@ Object.defineProperty(window, 'setup', setup);
 
 describe('Check removing todos', () => {
   test('Remove todos from the list', () => {
-    const doList = document.querySelector('.do-list');
+    const Tasks = jest.spyOn(Tasks, 'tasks');
+    // const doList = document.querySelector('.do-list');
     Tasks.saveTodo('To read about JavaScript');
     Tasks.saveTodo('Meeting with friends');
     Tasks.saveTodo('Meeting');
     Tasks.deleteTodo(0);
-    Tasks.renderTodos(doList);
+    Tasks.renderTodos(Tasks.doList);
     expect(todos).toHaveLength(2);
   });
 });
